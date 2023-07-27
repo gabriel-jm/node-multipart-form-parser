7fe9fd5279f0121bb194163cfae3d5aa270bd319
Gabriel José <gabriel.josemelo@hotmail.com>
2023-07-27 17:58:05 -0300

    add a big performance improvement

--- server-multipart.js
+++ server-multipart.js
@@ -4,7 +4,8 @@ const busboy = require('busboy')
 const fileFieldRegex = /\r\nContent-Disposition: form-data; name="(?<key>.+)"; filename="(?<name>.+)"\r\nContent-Type: (?<type>.+)\r\n\r\n/
 
 const server = http.createServer((req, res) => {
-  const boundary = req.headers['content-type'].split('; boundary=')[1]
+  const boundaryText = req.headers['content-type'].split('; boundary=')[1]
+  const boundary = `--${boundaryText}`
 
   if (req.url === '/busboy') {
     const busboyParser = busboy({ headers: req.headers })
@@ -53,42 +54,21 @@ const server = http.createServer((req, res) => {
       const fullBodyBuffer = Buffer.concat(body)
       body = fullBodyBuffer.toString('latin1')
 
-      const value = {}
+      let currentBufferIndex = boundary.length
 
-      const boundaryBuffer = Buffer.from(`--${boundary}`)
+      const value = {}
 
       for (const item of body.split(`--${boundary}`)) {
         if(item.includes('filename')) {
           const match = item.match(fileFieldRegex)
 
           const [matchText] = match
-          const contentBuffer = Buffer.from(item.substring(matchText.length).replace(/\r\n$/, ''))
-          const matchBuffer = Buffer.from(matchText)
-          const fieldTextLength = matchBuffer.length
-          
-          let startIndex = null
-          let data = null
+          const fieldTextLength = matchText.length
 
-          for(let b=0; b <= fullBodyBuffer.length; b++) {
-            if(!startIndex) {
-              const end = b + fieldTextLength
-              const subBuf = fullBodyBuffer.subarray(b, end)
-              
-              if(Buffer.compare(subBuf, matchBuffer) === 0) {
-                startIndex = end
-              }
-            }
-            
-            if(startIndex) {
-              const subBuf = fullBodyBuffer.subarray(b, b + boundary.length + 2)
+          const startIndex = currentBufferIndex + fieldTextLength
+          const data = fullBodyBuffer.subarray(startIndex, startIndex + item.length)
 
-              if(Buffer.compare(subBuf, boundaryBuffer) === 0) {
-                data = fullBodyBuffer.subarray(startIndex, b)
-                break
-              }
-            }
-          }
-
+          currentBufferIndex += startIndex + boundary.length
           const { groups } = match
 
           value[groups.key] = {
@@ -102,7 +82,8 @@ const server = http.createServer((req, res) => {
           )
 
           if(match) {
-            const { groups } = match
+            const { groups, input } = match
+            currentBufferIndex += (input.length + boundary.length)
 
             value[groups.key] = groups.value
           }
